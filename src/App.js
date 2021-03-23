783b33c9dfb70a71562d8cef77c4f51d821393ed
Shawn Fultz <shiftpoint32@gmail.com>
2021-03-23 17:30:08 -0400

    nudge

--- src/App.js
+++ src/App.js
@@ -5,4 +5,5 @@ import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => <Admin dataProvider={dataProvider} />;
 
+
 export default App;
