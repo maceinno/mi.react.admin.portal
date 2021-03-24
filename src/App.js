e60b551cac36bbb0f8d5899a1e638265a998f4de
Shawn Fultz <shiftpoint32@gmail.com>
2021-03-24 11:41:30 -0400

    adding posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import Dashboard from './Dashboard';
+import PostIcon from '@material-ui/icons/Book';
+import { PostList } from './Posts';
+
 
 import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
@@ -13,9 +16,10 @@ class App extends Component {
         hi
         <Admin dashboard={Dashboard} dataProvider={dataProvider}>
           <Resource name="users" list={ListGuesser} />
+          <Resource name="posts" list={PostList}  edit={EditGuesser}  icon={PostIcon} />
         </Admin>
       </div>
-      
+
     );
   }
 }
---
+++ src/Posts.js
@@ -0,0 +1,17 @@
+// in src/posts.js
+import * as React from "react";
+import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+
+export const PostList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <ReferenceField source="userId" reference="users">
+
+             <TextField source="name" />
+            </ReferenceField>
+            <TextField source="id" />
+            <TextField source="title" />
+            <TextField source="body" />
+        </Datagrid>
+    </List>
+);
