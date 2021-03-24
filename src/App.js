03e207d52bec6f2414254edcfc23a93920b874fa
Shawn Fultz <shiftpoint32@gmail.com>
2021-03-24 10:49:14 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import * as React from "react";
-import { Admin } from 'react-admin';
+import React, { Component } from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+
 import jsonServerProvider from 'ra-data-json-server';
-
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hi
+        <Admin dataProvider={dataProvider}>
+          <Resource name="users" list={ListGuesser} />
+        </Admin>
+      </div>
+    );
+  }
+}
 
 export default App;
